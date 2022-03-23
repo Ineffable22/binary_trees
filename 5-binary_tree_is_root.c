def412ba7640746a87621d19f0a5596f58cd0250
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-23 18:44:25 -0500

    Add: task 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,7 +7,6 @@
  * Return: return 1 if node is a root, otherwise 0
  *         If node is NULL, return 0
  */
-
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	return (!(node->parent) || !node);
