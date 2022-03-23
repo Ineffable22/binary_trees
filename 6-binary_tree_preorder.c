5b297bf5947087b1059bfe3859825bd57d3b49e9
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-23 18:55:34 -0500

    Update(7-binary_tree_inorder.c): restriction was changed

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -10,9 +10,8 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree || func)
+	if (!tree || !func)
 		return;
-
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
