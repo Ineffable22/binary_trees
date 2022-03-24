b03903bf861bd0736bbd64edcead30cc04a176c1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-24 12:17:06 -0500

    Update(16-binary_tree_is_perfect.c): new restriction was added

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,11 +49,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (binary_tree_balance(tree) != 0)
+		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
 	if (tree->right && tree->left)
 	{
 		return (binary_tree_is_perfect(tree->right) &&
-			binary_tree_is_perfect(tree->left) &&
-			!binary_tree_balance(tree->parent));
+			binary_tree_is_perfect(tree->left));
 	}
-	return (1);
+	return (0);
 }
