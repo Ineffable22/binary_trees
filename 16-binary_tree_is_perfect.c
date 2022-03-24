86c66da82c4dd1ba383c4dc354bec2d62637740a
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-23 20:15:58 -0500

    Update(16-binary_tree_is_perfect.c): function height was updated

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,11 +8,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t l = 0, r = 0;
+
+	if (!tree)
+		return (0);
+
 	if (tree->left)
-		return (1 + binary_tree_height(tree->left));
+		l = (1 + binary_tree_height(tree->left));
 	if (tree->right)
-		return (1 + binary_tree_height(tree->right));
-	return (0);
+		r = (1 + binary_tree_height(tree->right));
+	return (l > r ? l : r);
 }
 
 /**
