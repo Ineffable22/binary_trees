7a4410c2b4d304b54240013542cb67e3aa21bba3
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-24 13:40:02 -0500

    Update(binary_trees.h): new prototype was added

--- binary_trees.h
+++ binary_trees.h
@@ -54,6 +54,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void btlo_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
